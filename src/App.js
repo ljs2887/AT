f1d9cc953131d82a5618ac1cb9fabd55052f5397
ljs2887 <jio5402@naver.com>
2021-09-26 15:56:01 +0900

    gain

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
 import logo from './logo.svg';
+import { Navbar, Container, Nav } from 'react-bootstrap';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <div className="nav">
+        <div className="logo"></div>
+        <div className="home">
+            HOME
+        </div>
+        <div className="nav_item">
+            게시판
+        </div>
+        <div className="nav_item">
+            공지사항
+        </div>
+        <div className="nav_item">
+            건의사항
+        </div>
+        <div className="nav_item">
+            로그인
+        </div>
+      </div> */}
+      <Navbar bg="light" variant="light">
+        <Container>
+        <Navbar.Brand href="#home"><img src="./AT.png"></img></Navbar.Brand>
+        <Nav className="me-auto">
+          <Nav.Link href="#home">Home</Nav.Link>
+          <Nav.Link href="#게시판">게시판</Nav.Link>
+          <Nav.Link href="#공지사항">공지사항</Nav.Link>
+          <Nav.Link href="#건의사항">건의사항</Nav.Link>
+          <Nav.Link href="#로그인">로그인</Nav.Link>
+        </Nav>
+        </Container>
+      </Navbar>
+
+      {/* <div>
+        <div className="main_img">
+          
+        </div>
+      </div> */}
+
     </div>
   );
 }
